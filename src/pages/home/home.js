41069bc98107f1d237c2fde844fe2dfd94ce033d
Hajaniaina Andrianavalona <handri09@ambatovy.mg>
2022-06-20 21:17:58 +0300

    Demos on Date Picker

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,9 +4,10 @@ function Home() {
   return (
     <>
       <div className={`w-full max-w-screen-2xl mx-auto my-5`}>
-        <div className='text-center'>Home</div>
+        <div className=''></div>
+        {/* <div className='text-center'>Home</div>
         <p className='text-center border-2 border-green-500 '>Home</p>
-        <p className='text-center border-2 border-green-500 '>Home</p>
+        <p className='text-center border-2 border-green-500 '>Home</p> */}
       </div>
     </>
   )
