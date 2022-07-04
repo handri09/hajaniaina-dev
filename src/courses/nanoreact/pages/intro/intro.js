9fe8e3f4e982a6a235d40e6bea6fc0bb281b6ab7
Hajaniaina Andrianavalona <handri09@ambatovy.mg>
2022-07-04 15:54:15 +0300

    Apply a Template for course content

--- src/courses/nanoreact/pages/intro/intro.js
+++ src/courses/nanoreact/pages/intro/intro.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { CourseContent } from '../../../templates' 
+import { CourseContent } from '../../../templates';
+import src from '../../../assests/reactjs/01.intro/project01.png';
 
 function Intro() {
   return (
     <>
       <CourseContent
         navs={[
-          ['1. Tongasoa',''],
-          ["2. Inona 'lay hoamboarina", 'what'],
-          ['3. Ny Strategy', 'strategy'],
-          ['4. Ny Fisainana React', 'mindset'],
-          ['5. Ireo Fifanampiana', 'community'],
-          ['6. Fahazoana Fifanampiana', 'community2']
-        ]}/>
+          ['1. Tongasoa','', true],
+          ["2. Inona 'lay hoamboarina", 'what', false],
+          ['3. Ny Strategy', 'strategy', false],
+          ['4. Ny Fisainana React', 'mindset', false],
+          ['5. Ireo Fifanampiana', 'community', false],
+          ['6. Fahazoana Fifanampiana', 'community2', false]
+        ]}
+
+        data={[
+          ['title', 'Introduction'], 
+          ['p', `An welcoming word. Everything can come here, so lets be patient.An welcoming word. Everything can come here, so lets be patient. 
+          An welcoming word. Everything can come here, so lets be patient.`], 
+          ['code', 'Final code'], 
+          ['pic', src],
+          ['p', 'Mifarana atreto eh'],
+          ['video', 'https://www.youtube.com/embed/1bpLCkabaAg'],
+          ['p', 'Tongasoa Rajo'],
+          ['title', 'Titre ty'], 
+        ]}
+      />
     </>
   );
 }
